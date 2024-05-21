c7555cce2627df149b1097a0a5d8aff02c5e1c44
alalaalalalalal <justwon323@hanwha.com>
2024-05-21 10:48:07 +0900

    datacatAgent v1.0 realease

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"datacatAgent/ent/script"
 	"log"
 	"sync"
+	"time"
 
 	scriptpkg "datacatAgent/scriptExecutor"
 	"datacatAgent/util"
@@ -19,22 +20,24 @@ func main() {
 	defer client.Close()
 
 	//db.MigrateDB(client) // db 패키지의 MigrateDB 함수를 호출합니다
+	for true {
+		// usable이 1인 스크립트만 쿼리
+		scripts, err := client.Script.Query().
+			Where(script.Usable(1)).
+			All(context.Background())
+		if err != nil {
+			log.Fatalf("failed getting scripts: %v", err)
+		}
 
-	// usable이 1인 스크립트만 쿼리
-	scripts, err := client.Script.Query().
-		Where(script.Usable(1)).
-		All(context.Background())
-	if err != nil {
-		log.Fatalf("failed getting scripts: %v", err)
+		var wg sync.WaitGroup
+
+		for _, s := range scripts {
+			wg.Add(1) // 새로운 고루틴을 추가할 때마다 WaitGroup의 카운터를 증가
+			go scriptpkg.ExecuteScript(client, &wg, int(s.ID), s.Command, s.Comment)
+		}
+
+		wg.Wait() // 모든 고루틴이 완료될 때까지 대기
+		log.Println("All scripts executed")
+		time.Sleep(5 * 1000 * 60) // 5분에 한번씩 체크
 	}
-
-	var wg sync.WaitGroup
-
-	for _, s := range scripts {
-		wg.Add(1) // 새로운 고루틴을 추가할 때마다 WaitGroup의 카운터를 증가
-		go scriptpkg.ExecuteScript(client, &wg, int(s.ID), s.Command, s.Comment)
-	}
-
-	wg.Wait() // 모든 고루틴이 완료될 때까지 대기
-	log.Println("All scripts executed")
 }
