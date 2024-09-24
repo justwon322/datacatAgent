85fdef3b54f59284577b8781b38693737cae008f
justwon322 <justwon322@gmail.com>
2024-09-24 11:32:55 +0900

    Update qcstsp_thesis.go

--- qcstsp_thesis.go
+++ qcstsp_thesis.go
@@ -14,14 +14,14 @@ func thesis() {
 	browser := rod.New().ControlURL(url).MustConnect()
 
 	// 새 탭을 열고 웹페이지 로딩
-	pageURL := "https://qcstsp.qcells.com/login-dev.html" // 원하는 URL로 변경하세요.
+	pageURL := "<CHANGE WHAT YOU WANT>" // 원하는 URL로 변경하세요.
 	page := browser.MustPage(pageURL)
 
 	id := "#user_id"                  // 비밀번호 입력 필드의 선택자
 	loginButton := "#login"           // 로그인 버튼의 선택자
 	searchKeyword := "#input-keyword" // 검색키워드 필드 선택자
 
-	page.MustElement(id).MustInput("mshwang_qc")
+	page.MustElement(id).MustInput("<ID>")
 
 	// 로그인 버튼 클릭
 	page.MustElement(loginButton).MustClick()
@@ -32,10 +32,10 @@ func thesis() {
 	// 웹페이지의 스크린샷 생성
 	now := time.Now()
 	date := now.Format("20060102")
-	//page.MustWaitLoad().MustScreenshotFullPage("/Users/jamesyu/Downloads/qcptsp_" + date + "_논문상세.png")
+	//page.MustWaitLoad().MustScreenshotFullPage("<CHANGE WHAT YOU WANT>")
 
-	page = browser.MustPage("https://qcptsp.qcells.com/view/thesis/JN-OA-0064709")
-	page.MustWaitLoad().MustScreenshotFullPage("C:/Users/bsgdev/DailyRiskManagement/qcptsp_" + date + "_논문상세.png")
+	page = browser.MustPage("<CHANGE WHAT YOU WANT>")
+	page.MustWaitLoad().MustScreenshotFullPage("<CHANGE WHAT YOU WANT>")
 	log.Println("Succeeded")
 	// 리소스 정리
 	browser.MustClose()
